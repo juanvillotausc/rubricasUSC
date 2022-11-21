7afda564aeebe44c41886b9187db511616701717
juanvidev <juanvillota33@gmail.com>
2022-11-21 10:14:01 -0500

    ajuste deploy .sh

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:juanvillotausc/juanvillotausc.github.io.git main
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 #git push -f git@github.com:juanvillotausc/rubricasUSC.git main:gh-pages
